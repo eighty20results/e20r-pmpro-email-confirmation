b024f1f80d9b5533686f59d8317649a63c8d36dd
Thomas Sjolshagen <thomas@eighty20results.com>
2019-09-13 11:56:47 -0600

    BUG FIX: No need to sleep for 30 seconds when importing test DB

--- docker-env/import-db.sh
+++ docker-env/import-db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PROJECT_NAME='e20r-pmpro-email-confirmation'
 echo "Importing database for ${PROJECT_NAME}"
-sleep 30;
+# sleep 30;
 echo $(pwd)
 make wp db import ./mariadb-init/${PROJECT_NAME}.sql
